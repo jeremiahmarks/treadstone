0e36fcd9c4cb883438ec9aaa92375c7183ad5549
Jeremiah Marks <Jeremiah@JLMarks.org>
2015-09-26 19:52:21 -0700

    the real start of things.

---
+++ _local.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Jeremiah Marks
+# @Date:   2015-09-26 19:45:51
+# @Last Modified 2015-09-26 Jeremiah@JLMarks.org
+# @Last Modified time: 2015-09-26 19:49:30
+
+settings={}
+settings['username'] = 'infusionsoftId@example.com'
+settings['password'] = 'yourinfusionsoftpassword'
+settings['appname'] = 'yourappname'
+settings['apikey'] = 'somereallylongstring'
+settings['inputcsv'] = '/full/path/to/thefile.csv'
