1dabc98ac7b8d09cb71351b14c64612e283577c6
Jeremiah Marks <Jeremiah@JLMarks.org>
2015-09-30 15:51:25 -0700

    From home

---
+++ functionsImplemented.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Jeremiah Marks
+# @Date:   2015-09-28 19:34:35
+# @Last Modified 2015-09-28 Jeremiah@JLMarks.org
+# @Last Modified time: 2015-09-28 21:13:43
+
+#Since I did not push my changes from work, this is going to be some of the functions that
+# need to be implemented.
+
+# Gui stuff is what is needed most.
+
